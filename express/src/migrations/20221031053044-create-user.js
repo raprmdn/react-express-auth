811a093e569386dd8cfde2460631dee9790d47f1
raprmdn <rafiputraramadhan14@gmail.com>
2022-10-31 12:33:31 +0700

    User model and migration.

---
+++ express/src/migrations/20221031053044-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            username: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            provider: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue: 'local',
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Users');
+    }
+};
